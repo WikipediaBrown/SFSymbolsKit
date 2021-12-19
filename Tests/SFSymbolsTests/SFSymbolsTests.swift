5615f26713f7ee6abd126090337244dff27cbb4c
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2021-12-19 03:10:00 -0800

    first commit

---
+++ Tests/SFSymbolsTests/SFSymbolsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SFSymbols
+
+final class SFSymbolsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(SFSymbols.text, "Hello, World!")
+    }
+}
---
+++ generate.py
@@ -0,0 +1,61 @@
+
+numbers = {
+  "case": "caseImage",
+  "repeat": "repeatImage",
+  "return": "returnImage",
+  "0": "zero",
+  "1": "one",
+  "2": "two",
+  "3": "three",
+  "4": "four",
+  "5": "five",
+  "6": "six",
+  "7": "seven",
+  "8": "eight",
+  "9": "nine"
+}
+
+file = open("Sources/SFSymbols/SFSymbols.swift", "w")
+
+file.write("//\n")
+file.write("//  SFSymbols.swift\n")
+file.write("//  Missions\n")
+file.write("//\n")
+file.write("//  Created by nonplus on 12/18/21.\n")
+file.write("//\n")
+file.write("\n")
+file.write("import UIKit\n")
+file.write("\n")
+file.write("extension UIImage {\n")
+file.write("\n")
+
+with open('SFSymbols.txt') as topo_file:
+    for line in topo_file:
+        parts = line.split('.')
+        camelCaseName = ""
+        
+        for part in parts:
+            if part == parts[0]:
+            
+                if part.rstrip() in numbers:
+                    camelCaseName += numbers[part.rstrip()]
+                    continue
+                elif part[0] in numbers:
+                    camelCaseName += numbers[part[0]] + part[1:]
+                    continue
+                camelCaseName += part
+                continue
+            camelCaseName += part.capitalize()
+            
+        result = "    static let {} = getSystemImage(named: \"{}\")\n".format(camelCaseName.rstrip(), line.rstrip())
+        file.write(result)
+        
+file.write("\n")
+file.write("    static func getSystemImage(named: String) -> UIImage {\n")
+file.write("        guard let image = UIImage(systemName: named)\n")
+file.write("        else { fatalError(\"Could Not Find System Image\") }\n")
+file.write("        return image\n")
+file.write("    }\n")
+file.write("\n")
+file.write("}\n")
+file.close()
