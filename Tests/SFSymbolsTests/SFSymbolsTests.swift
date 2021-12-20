b838389b8e98f014247cc37e7ff7e158fb043655
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2021-12-19 17:05:59 -0800

    Removed Fatal Error

--- Tests/SFSymbolsTests/SFSymbolsTests.swift
+++ Tests/SFSymbolsTests/SFSymbolsTests.swift
@@ -3,6 +3,12 @@ import XCTest
 
 final class SFSymbolsTests: XCTestCase {
     func testExample() throws {
+        for symbol in SFSymbols.allCases {
+            XCTAssertTrue(symbol.image is UIImage, <#T##message: String##String#>)
+            
+            
+            
+        }
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
