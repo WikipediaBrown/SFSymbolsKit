286a281a553c9900a54a31aed9733f41f819463c
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2021-12-28 22:35:38 -0800

    new fangled

--- Tests/SFSymbolsTests/SFSymbolsTests.swift
+++ Tests/SFSymbolsTests/SFSymbolsTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class SFSymbolsTests: XCTestCase {
     func test_SFSymbols_Enum_image_found() {
         for symbol in SFSymbols.allCases {
-            XCTAssertNotEqual(symbol.image, UIImage(), "SFSymbols Image not found.")
+            XCTAssertNotEqual(symbol.image, UIImage(), "SFSymbol (\(symbol.rawValue)) Image not found.")
         }
     }
 }
