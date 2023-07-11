025f9b40a3797f9539c97baf3d48e1697051736d
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2023-07-11 03:58:30 -0700

    Updated

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-xcodebuild docbuild -scheme SFSymbols \
-    -destination generic/platform=iOS \
-    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path SFSymbols --output-path docs" \
-    DOCC_OUTPUT_DIR=./docs
+#xcodebuild docbuild -scheme SFSymbols \
+#    -destination generic/platform=iOS \
+#    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path SFSymbols --output-path docs" \
+#    DOCC_OUTPUT_DIR=./docs
     
 xcodebuild docbuild -scheme SFSymbols \
     -destination generic/platform=macOS \
