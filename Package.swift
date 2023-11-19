2ce6a1e1325acde62ac0ce0cb83071030fb8aa18
Wikipedia Brown <WikipediaBrown@users.noreply.github.com>
2023-11-18 16:10:15 -0800

    more

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SFSymbols",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
